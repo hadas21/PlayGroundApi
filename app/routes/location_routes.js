f5ec548d7dba42ec66874a09fa4df96017542ed3
hadas21 <hadas.axel@gmail.com>
2021-08-24 15:46:14 -0500

    complete location routes

--- app/routes/location_routes.js
+++ app/routes/location_routes.js
@@ -3,12 +3,10 @@ const passport = require('passport')
 
 const Location = require('../models/location')
 
-
 const customErrors = require('../../lib/custom_errors')
 
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
-
 
 const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
@@ -18,8 +16,7 @@ const router = express.Router()
 // INDEX
 // GET /locations
 router.get('/locations', requireToken, (req, res, next) => {
-  
-    Location.find({ owner: req.user.id })
+  Location.find({ owner: req.user.id })
     .then(locations => {
       return locations.map(location => location.toObject())
     })
@@ -30,8 +27,7 @@ router.get('/locations', requireToken, (req, res, next) => {
 // SHOW
 // GET /locations/5a7db6c74d55bc51bdf39793
 router.get('/locations/:id', requireToken, (req, res, next) => {
-  
-    Location.findById(req.params.id)
+  Location.findById(req.params.id)
     .then(handle404)
     .then(location => res.status(200).json({ location: location.toObject() }))
     .catch(next)
@@ -39,14 +35,29 @@ router.get('/locations/:id', requireToken, (req, res, next) => {
 
 // CREATE
 // POST /locations
-router.post('/locations', requireToken, (req, res, next) => {
-  // set owner of new location to be current user
-  req.body.location.owner = req.user.id
+// router.post('/locations', requireToken, (req, res, next) => {
+//   // set owner of new location to be current user
+//   req.body.location.owner = req.user.id
 
-  Location.create(req.body.location)
-    .then(location => {
-      res.status(201).json({ location: location.toObject() })
-    })
+//   Location.create(req.body.location)
+//     .then(location => {
+//       res.status(201).json({ location: location.toObject() })
+//     })
+//     .catch(next)
+// })
+
+router.post('/locations', requireToken, (req, res, next) => {
+  // get location data from request
+  const location = req.body.location
+
+  // Attach the owner using the `req.user.id`
+  location.owner = req.user.id
+
+  // save location to mongodb
+  Location.create(location)
+  // if successful respond with 201 and location json
+    .then((location) => res.status(201).json({ location: location.toObject() }))
+  // on error respond with 500 and error message
     .catch(next)
 })
 
@@ -68,8 +79,7 @@ router.patch('/locations/:id', requireToken, removeBlanks, (req, res, next) => {
 // DESTROY
 // DELETE /locations/5a7db6c74d55bc51bdf39793
 router.delete('/locations/:id', requireToken, (req, res, next) => {
-  
-    Location.findById(req.params.id)
+  Location.findById(req.params.id)
     .then(handle404)
     .then(location => {
       requireOwnership(req, location)
