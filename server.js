5e54d644a8064aba3a71a12c2f23bf01cd369074
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-06 11:21:31 +0200

    fix cors

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const app = express();
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 
-app.use(cors);
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
